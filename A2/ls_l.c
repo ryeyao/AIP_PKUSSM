de5c1dde5c5e15d0ee77ab14f1473f10bc83ed79
Rye Yao <rye.y.cn@gmail.com>
2013-04-03 20:46:34 +0800

    task 1 finished

--- A2/ls_l.c
+++ A2/ls_l.c
@@ -3,6 +3,8 @@
 #include<sys/stat.h>
 #include<unistd.h>
 #include<dirent.h>
+#include<pwd.h>
+#include<grp.h>
 
 
 #ifndef LS_L_C
@@ -11,6 +13,31 @@
 
 #define EXIT_FAILURE -1
 #define EXIT_SUCCESS 0
+
+char* uid_to_name(uid_t uid) {
+    struct passwd *getpwuid(), *pw_ptr;
+    static char numstr[10];
+
+    if ((pw_ptr = getpwuid(uid)) == NULL) {
+        
+        sprintf(numstr, "%d", uid);
+        return numstr;
+    } else {
+        return pw_ptr->pw_name;
+    }
+}
+
+char* gid_to_name(gid_t gid) {
+    struct group *getgrgid(), *grp_ptr;
+    static char numstr[10];
+
+    if ((grp_ptr = getgrgid(gid)) == NULL) {
+        sprintf(numstr, "%d", gid);
+        return numstr;
+    } else {
+        return grp_ptr->gr_name;
+    }
+}
 
 char* get_path(char* name) {
     char* current_dir;
@@ -31,10 +58,68 @@ char* get_path(char* name) {
 
 }
 
+// make the permission string
+char* perm_str(mode_t mode) {
+
+    char* mode_str = malloc(9);
+    strcpy(mode_str, "rwxrwxrwx");
+    mode_t usr_perm, grp_perm, oth_perm;
+
+    usr_perm = mode & S_IRWXU;
+    grp_perm = mode & S_IRWXG;
+    oth_perm = mode & S_IROTH;
+
+    // User perm
+    if (!(usr_perm & S_IRUSR)) {
+        mode_str[0] = '-';
+    }
+
+    if (!(usr_perm & S_IWUSR)) {
+        mode_str[1] = '-';
+    }
+
+    if (!(usr_perm & S_IXUSR)) {
+        mode_str[2] = '-';
+    }
+
+    // Group
+    if (!(grp_perm & S_IRGRP)) {
+        mode_str[3] = '-';
+    }
+
+    if (!(grp_perm & S_IRGRP)) {
+        mode_str[4] = '-';
+    }
+
+    if (!(grp_perm & S_IRGRP)) {
+        mode_str[5] = '-';
+    }
+
+    // Other
+    if (!(oth_perm & S_IROTH)) {
+        mode_str[6] = '-';
+    }
+
+    if (!(oth_perm & S_IWOTH)) {
+        mode_str[7] = '-';
+    }
+
+    if (!(oth_perm & S_IXOTH)) {
+        mode_str[8] = '-';
+    }
+
+    return mode_str;
+
+}
+
+// Print the file information the way "ls -l" does.
 void info_file(char* name) {
     struct stat sb;
     char* path;
 
+    if (!strcmp(name, ".") || !strcmp(name, "..")) {
+        return;
+    }
     path = get_path(name);
 
     if (stat(path, &sb) == -1) {
@@ -42,21 +127,54 @@ void info_file(char* name) {
         exit(EXIT_FAILURE);
     }
 
-    char mode[9];
-    int prop_1;
-    char usr_name[128];
-    char usr_name_2[128];
-    long block_size;
-    char last_mod[128];
+    char type;
+    char *perm;
+    int nlink;
+    char* usr_name;
+    char* usr_group_name;
+    long size;
+    char mtime[128];
 
-    block_size = (long) sb.st_blksize;
-    strcpy(last_mod, ctime(&sb.st_mtime));
+    type = '-';
+    perm = perm_str(sb.st_mode);
+    nlink = sb.st_nlink;
+    usr_name = uid_to_name(sb.st_uid);
+    usr_group_name = gid_to_name(sb.st_gid);
+    size = (long) sb.st_blksize;
+    strcpy(mtime, ctime(&sb.st_mtime));
+    int end = strlen(mtime);
+    mtime[end - 2] = '\0'; // remove the '\n'
 
-    printf("%9s %1d %s %s    %ld  %s  %s \r\n", mode, prop_1, usr_name, usr_name_2, block_size, last_mod, name);
+    switch (sb.st_mode & S_IFMT) {
+       case S_IFBLK:  
+           type = 'b';
+           break;
+       case S_IFCHR:  
+           type = 'c';
+           break;
+       case S_IFDIR:  
+           type = 'd';
+           break;
+       case S_IFIFO:
+           type = 'f';
+           break;
+       case S_IFLNK:
+           type = 'l';
+           break;
+       case S_IFREG:
+           type = 'r';
+           break;
+       case S_IFSOCK:
+           type = 's';
+           break;
+       default:
+           break;
+    }
 
+    printf("%c%9s %1d %s %s %ld %s %s\n", type, perm, nlink, usr_name, usr_group_name, size, mtime, name);
 
     free(path);
-
+    free(perm);
 }
 
 int main(int argc, char** argv) {
@@ -80,14 +198,9 @@ int main(int argc, char** argv) {
         perror("scandir");
     } else {
         while (n--) {
-            //printf("%s\n", namelist[n]->d_name);
             info_file(namelist[n]->d_name);
             free(namelist[n]);
         }
         free(namelist);
     }
-
 }
-
-
-
